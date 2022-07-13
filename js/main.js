3def95a41585171b92df2f5e524e943348b527cd
Trần Hữu Phúc <30775322+Tranhuuphuc123@users.noreply.github.com>
2022-07-14 01:45:42 +0700

    luu hoan chinh

--- js/main.js
+++ js/main.js
@@ -107,13 +107,5 @@ sr.reveal(`.about__img-overlay,
     interval: 100,
 })
 
-/* phần xây dựng bản đồ */
-function myMap() {
-    var mapOptions = {
-    center: new google.maps.LatLng(-33.891049654862904, 151.27742942890129),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
+
     
